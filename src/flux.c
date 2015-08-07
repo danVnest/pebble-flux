ee425c833130f5e3b126790c08c76909cb32e6ee
danVnest <daniel.vogelnest@me.com>
2015-08-07 15:54:42 +1000

    Altered text layer to add clarifying text outline

--- src/flux.c
+++ src/flux.c
@@ -1,9 +1,10 @@
 #include "flux.h"
-
+	
 static Window *window;
 static Layer *background_layer;
-static TextLayer *time_layer;
-static TextLayer *date_layer;
+static Layer *text_layer;
+static char time_text[] = "00:00";
+static char date_text[] = "MON 11 JAN";
 static GFont time_font;
 static GFont date_font;
 
@@ -14,57 +15,59 @@ static void initialise();
 static void cleanup();	
 
 static void update_time(struct tm *tick_time) {
-	static char time_buffer[] = "00:00";
-	if(clock_is_24h_style() == false) strftime(time_buffer, sizeof("00:00"), "%l:%M", tick_time);
-	else strftime(time_buffer, sizeof("00:00"), "%k:%M", tick_time);
-	text_layer_set_text(time_layer, time_buffer);
+	if(clock_is_24h_style() == false) strftime(time_text, sizeof("00:00"), "%l:%M", tick_time);
+	else strftime(time_text, sizeof("00:00"), "%k:%M", tick_time);
+	layer_mark_dirty(text_layer);
 }
 
 void update_date(struct tm *tick_time) {
-	static char date_buffer[] = "MON 11 JAN";
-	uint8_t date_setting = get_setting(SETTING_DATE);
-	if (date_setting == 0) text_layer_set_text(date_layer, " ");
-	else {
-		if (date_setting == 1) strftime(date_buffer, sizeof("MON 11 JAN"), "%a %e %b", tick_time);
-		else strftime(date_buffer, sizeof("MON JAN 11"), "%a %b %e", tick_time);
-		text_layer_set_text(date_layer, date_buffer);
-	}
+	if (get_setting(SETTING_DATE) == 1) strftime(date_text, sizeof("MON 11 JAN"), "%a %e %b", tick_time);
+	else if (get_setting(SETTING_DATE) == 2) strftime(date_text, sizeof("MON JAN 11"), "%a %b %e", tick_time);
+	update_time(tick_time);
+}
+
+void draw_text(Layer *layer, GContext *ctx) {
+	graphics_context_set_text_color(ctx, GColorBlack);
+	graphics_draw_text(ctx, time_text, time_font, GRect(0, 54 - 2, 140, 40), GTextOverflowModeFill, GTextAlignmentCenter, NULL);
+	graphics_draw_text(ctx, time_text, time_font, GRect(0, 54 + 2, 140, 40), GTextOverflowModeFill, GTextAlignmentCenter, NULL);
+	graphics_draw_text(ctx, time_text, time_font, GRect(4, 54 - 2, 140, 40), GTextOverflowModeFill, GTextAlignmentCenter, NULL);
+	graphics_draw_text(ctx, time_text, time_font, GRect(4, 54 + 2, 140, 40), GTextOverflowModeFill, GTextAlignmentCenter, NULL);
+	if (get_setting(SETTING_DATE) != 0) {
+		graphics_draw_text(ctx, date_text, date_font, GRect(0, 140 - 1, 140, 20), GTextOverflowModeFill, GTextAlignmentCenter, NULL);
+		graphics_draw_text(ctx, date_text, date_font, GRect(0, 140 + 1, 140, 20), GTextOverflowModeFill, GTextAlignmentCenter, NULL);
+		graphics_draw_text(ctx, date_text, date_font, GRect(2, 140 - 1, 140, 20), GTextOverflowModeFill, GTextAlignmentCenter, NULL);
+		graphics_draw_text(ctx, date_text, date_font, GRect(2, 140 + 1, 140, 20), GTextOverflowModeFill, GTextAlignmentCenter, NULL);
+	}	
+	graphics_context_set_text_color(ctx, GColorWhite);
+	graphics_draw_text(ctx, time_text, time_font, GRect(2, 54, 140, 40), GTextOverflowModeFill, GTextAlignmentCenter, NULL);
+	if (get_setting(SETTING_DATE) != 0) graphics_draw_text(ctx, date_text, date_font, GRect(1, 140, 142, 20), GTextOverflowModeFill, GTextAlignmentCenter, NULL);
 }
 
 static void tick_handler(struct tm *tick_time, TimeUnits units_changed) {
-	update_time(tick_time);
 	if (units_changed & DAY_UNIT) update_date(tick_time);
+	else update_time(tick_time);
 }
 
 static void window_load(Window *window) {
 	window_set_background_color(window, GColorBlack);
 	background_layer = layer_create(GRect(0, 0, 144, 168));
-	layer_set_update_proc(background_layer, draw_nodes);
+	text_layer = layer_create(GRect(0, 0, 144, 168));
 	create_nodes();
-	layer_add_child(window_get_root_layer(window), background_layer);
-	time_layer = text_layer_create(GRect(0, 54, 144, 40));
-	date_layer = text_layer_create(GRect(0, 140, 144, 20));
-	time_font = fonts_load_custom_font(resource_get_handle(RESOURCE_ID_FONT_IMAGINE_38));
-	date_font = fonts_load_custom_font(resource_get_handle(RESOURCE_ID_FONT_IMAGINE_18));
-	text_layer_set_font(time_layer, time_font);
-	text_layer_set_font(date_layer, date_font);
-	text_layer_set_text_color(time_layer, GColorWhite);
-	text_layer_set_text_color(date_layer, GColorWhite);
-	text_layer_set_background_color(time_layer, GColorClear);
-	text_layer_set_background_color(date_layer, GColorClear);
-	text_layer_set_text_alignment(time_layer, GTextAlignmentCenter);
-	text_layer_set_text_alignment(date_layer, GTextAlignmentCenter);
-	layer_add_child(window_get_root_layer(window), text_layer_get_layer(time_layer));
-	layer_add_child(window_get_root_layer(window), text_layer_get_layer(date_layer));
 	time_t raw_time = time(NULL);
 	struct tm *tick_time = localtime(&raw_time);
 	update_time(tick_time);
 	update_date(tick_time);
+	time_font = fonts_load_custom_font(resource_get_handle(RESOURCE_ID_FONT_IMAGINE_38));
+	date_font = fonts_load_custom_font(resource_get_handle(RESOURCE_ID_FONT_IMAGINE_18));
+	layer_set_update_proc(background_layer, draw_nodes);
+	layer_set_update_proc(text_layer, draw_text);
+	layer_add_child(window_get_root_layer(window), background_layer);
+	layer_add_child(window_get_root_layer(window), text_layer);
 }
 
 static void window_unload(Window *window) {
-	text_layer_destroy(time_layer);
-	text_layer_destroy(date_layer);
+	layer_destroy(background_layer);
+	layer_destroy(text_layer);
 	fonts_unload_custom_font(time_font);
 	fonts_unload_custom_font(date_font);
 }
