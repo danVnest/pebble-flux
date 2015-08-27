9c75d512ea2826061d3c6018b77a01f8df0ae1cb
Daniel Vogelnest <daniel.vogelnest@me.com>
2015-08-27 12:36:46 +1000

    added lost bluetooth connection icon

--- src/flux.c
+++ src/flux.c
@@ -4,13 +4,15 @@ static Window *window;
 static Layer *background_layer;
 static Layer *text_layer;
 static Layer *battery_layer;
+static Layer *bluetooth_layer;
 static char time_text[] = "00:00";
 static char date_text[] = "MON 11 JAN";
-static bool show_battery;
 static GFont time_font;
 static GFont date_font;
-
+static bool show_battery;
+static bool show_bluetooth;
 static void update_time(struct tm *tick_time);
+static void update_bluetooth(bool show);
 static void window_load(Window *window);
 static void window_unload(Window *window);
 static void initialise();
@@ -31,6 +33,11 @@ void update_date(struct tm *tick_time) {
 void update_battery(bool show) {
 	show_battery = show;
 	layer_mark_dirty(battery_layer);
+}
+
+static void update_bluetooth(bool show) {
+	show_bluetooth = show;
+	layer_mark_dirty(bluetooth_layer);
 }
 
 void draw_text(Layer *layer, GContext *ctx) {
@@ -72,14 +79,29 @@ void draw_battery(Layer *layer, GContext *ctx) {
 		graphics_context_set_text_color(ctx, GColorWhite);
 		graphics_draw_text(ctx, percent_text, date_font, GRect(4, -2, 44, 20), GTextOverflowModeFill, GTextAlignmentCenter, NULL);
 	}
-	else {
+}
+
+void draw_bluetooth(Layer *layer, GContext *ctx) {
+	if (show_bluetooth) { 
 		graphics_context_set_fill_color(ctx, GColorBlack);
-		graphics_fill_rect(ctx, GRect(0, 0, 48, 21), 0, GCornerNone);
-		graphics_fill_rect(ctx, GRect(48, 6, 2, 9), 0, GCornerNone);
+		graphics_fill_rect(ctx, GRect(0, 4, 21, 40), 0, GCornerNone);
 		graphics_context_set_fill_color(ctx, GColorWhite);
-		graphics_fill_rect(ctx, GRect(1, 1, 46, 19), 0, GCornerNone);
-		graphics_fill_rect(ctx, GRect(47, 7, 2, 7), 0, GCornerNone);
+		graphics_fill_rect(ctx, GRect(1, 5, 19, 38), 0, GCornerNone);
+		graphics_context_set_fill_color(ctx, GColorBlack);
+		graphics_fill_rect(ctx, GRect(3, 11, 15, 26), 0, GCornerNone);
+		graphics_fill_rect(ctx, GRect(9, 39, 3, 2), 0, GCornerNone);
+		graphics_context_set_antialiased(ctx, false);
+		graphics_context_set_stroke_color(ctx, GColorBlack);
+		graphics_context_set_stroke_width(ctx, 5);
+		graphics_draw_line(ctx, GPoint(2, 2), GPoint(19, 46));
+		graphics_context_set_stroke_color(ctx, GColorWhite);
+		graphics_context_set_stroke_width(ctx, 1);
+		graphics_draw_line(ctx, GPoint(2, 2), GPoint(19, 46));
 	}
+}
+
+void bluetooth_handler(bool connected) {
+	update_bluetooth(!connected);
 }
 
 void tick_handler(struct tm *tick_time, TimeUnits units_changed) {
@@ -97,6 +119,7 @@ static void window_load(Window *window) {
 	background_layer = layer_create(GRect(0, 0, 144, 168));
 	text_layer = layer_create(GRect(0, 0, 144, 168));
 	battery_layer = layer_create(GRect(8, 8, 50, 21));
+	bluetooth_layer = layer_create(GRect(144 - 21 - 8, 4, 21, 50));
 	create_nodes();
 	time_t raw_time = time(NULL);
 	struct tm *tick_time = localtime(&raw_time);
@@ -107,14 +130,18 @@ static void window_load(Window *window) {
 	layer_set_update_proc(background_layer, draw_nodes);
 	layer_set_update_proc(text_layer, draw_text);
 	layer_set_update_proc(battery_layer, draw_battery);
+	layer_set_update_proc(bluetooth_layer, draw_bluetooth);
 	layer_add_child(window_get_root_layer(window), background_layer);
 	layer_add_child(window_get_root_layer(window), text_layer);
 	layer_add_child(window_get_root_layer(window), battery_layer);
+	layer_add_child(window_get_root_layer(window), bluetooth_layer);
 }
 
 static void window_unload(Window *window) {
 	layer_destroy(background_layer);
 	layer_destroy(text_layer);
+	layer_destroy(battery_layer);
+	layer_destroy(bluetooth_layer);
 	fonts_unload_custom_font(time_font);
 	fonts_unload_custom_font(date_font);
 }
@@ -127,11 +154,15 @@ static void initialise() {
 		.unload = window_unload
 	});
 	window_stack_push(window, true);
+	bluetooth_connection_service_subscribe(bluetooth_handler);
 	begin_startup_animation();
 }
 
 static void cleanup() {
+	wakeup_cancel_all();
 	tick_timer_service_unsubscribe();
+	battery_state_service_unsubscribe();
+	bluetooth_connection_service_unsubscribe();
 	window_destroy(window);
 	save_settings();
 }
